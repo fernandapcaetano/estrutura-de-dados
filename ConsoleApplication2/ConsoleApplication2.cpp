67304c05928295047403f62da5df6a0e4435c480
Maria Fernanda P Caetano <fernandamariacaetanopereira@gmail.com>
2023-02-28 12:55:44 -0300

    mÉDIA DA nOTA

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+// ConsoleApplication2.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float p1 = 0; 
+	float p2 = 0;
+	float media = 0;
+
+   cout << "MEDIA DA NOTA\n";
+   cout << "==============\n\n";
+   cout << "Digite a nota da P1\n";
+   cin  >> p1;
+   cout << "Digite a nota da P2\n";
+   cin >> p2;
+
+   media = (p1 + p2) / 2;
+
+   if (media >= 6)
+   {
+	   cout << "APROVADO\n";
+   }
+   else
+   {
+	   cout << "REPROVADO\n";
+   }
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
